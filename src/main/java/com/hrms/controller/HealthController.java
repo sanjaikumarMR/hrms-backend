cb4321f28ba51bd47a8d05f2eb42c262c8e9c8d0
sanjai <146710527+sanjaikumarMR@users.noreply.github.com>
2025-07-26 14:05:11 +0530

    HealthController.java

--- src/main/java/com/hrms/controller/HealthController.java
+++ src/main/java/com/hrms/controller/HealthController.java
@@ -1,4 +1,14 @@
-@RestController
+package com.hrms.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.hrms.service.FaceService;
+import com.hrms.model.FaceRegisterRequest;
+
+import java.util.List;
+
+
 public class HealthController {
 
     @GetMapping("/ping")
